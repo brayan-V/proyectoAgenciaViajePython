50c610f04772af19ad60d906c23c10c143198470
Brayan Velasco de la Ossa <bvelasco10b@gmail.com>
2024-09-01 04:08:14 -0500

    Se desarrollan las funciones reservar vuelos y reservar hoteles.

--- agencia_viaje.py
+++ agencia_viaje.py
@@ -1,41 +1,49 @@
 vuelos = {
-    1: {"Ciudad": "Bogota", "salidas": {"a": 130.000, "b": 140.000, "c": 150.000}},
-    2: {"Ciudad": "Medellin", "salidas": {"a": 145.000, "b": 155.000, "c": 165.000}},
-    3: {"Ciudad": "Santa Marta", "salidas": {"a": 160.000, "b": 170.000,  "c": 180.000}},
-    4: {"Ciudad": "Amazonas", "salidas": {"a": 175.000, "b": 185.000, "c": 200.000}}
+    1: {"Ciudad": "Bogota", "salidas": {"a": 130000, "b": 140000, "c": 150000}},
+    2: {"Ciudad": "Medellin", "salidas": {"a": 145000, "b": 155000, "c": 165000}},
+    3: {"Ciudad": "Santa Marta", "salidas": {"a": 160000, "b": 170000,  "c": 180000}},
+    4: {"Ciudad": "Amazonas", "salidas": {"a": 175000, "b": 185000, "c": 200000}}
 }
 
 hoteles = {
     1: {
-        1:{"Nombre": "Hotel arena suites 4 dias - 3 noches", "Tours": "La Candelaria, Monserrate y Museo del Oro Bogotá City Tour", "precio": 643.000 },
-        2: {"Nombre": "Hotel Radisson Bogota Metrotel 4 dias - 3 noches", "Tours": "Bogota Tour Privado Catedral de Sal Zipaquira + Almuerzo", "precio": 999.999},
-        3: {"Nombre": "Hotel Radel Bogotá  3 dias - 2 noches", "Tours": "Tour gastronómico por el Viejo Bogotá con más de 12 degustaciones", "precio": 415.000  }
+        1:{"Nombre": "Hotel arena suites 4 dias - 3 noches", "Tours": "La Candelaria, Monserrate y Museo del Oro Bogotá City Tour", "precio": 643000 },
+        2: {"Nombre": "Hotel Radisson Bogota Metrotel 4 dias - 3 noches", "Tours": "Bogota Tour Privado Catedral de Sal Zipaquira + Almuerzo", "precio": 999999},
+        3: {"Nombre": "Hotel Radel Bogotá  3 dias - 2 noches", "Tours": "Tour gastronómico por el Viejo Bogotá con más de 12 degustaciones", "precio": 415000  }
     },
     2: {
-        1:{"Nombre": "Hotel Sie7e 4 dias - 3 noches", "Tours": "Tour a Guatape, Piedra del Peñol con Recorrido en Barco, Desayuno y Almuerzo", "precio": 570.000},
-        2:{"Nombre": "Hotel Med Laureles 3 dias - 2 noches", "Tours": "City tour Medellin + Comuna 13 + Graffititour Se paisa por un dia", "precio": 572.000 },
-        3:{"Nombre": "Stanza Hotel Medellin 4 dias - 3 noches", "Tours": "Excursión a plantación de café y paseo a caballo: Todo en un gran e inolvidable día", "precio": 999.999}
+        1:{"Nombre": "Hotel Sie7e 4 dias - 3 noches", "Tours": "Tour a Guatape, Piedra del Peñol con Recorrido en Barco, Desayuno y Almuerzo", "precio": 570000},
+        2:{"Nombre": "Hotel Med Laureles 3 dias - 2 noches", "Tours": "City tour Medellin + Comuna 13 + Graffititour Se paisa por un dia", "precio": 572000 },
+        3:{"Nombre": "Stanza Hotel Medellin 4 dias - 3 noches", "Tours": "Excursión a plantación de café y paseo a caballo: Todo en un gran e inolvidable día", "precio": 999999}
     },
     3: {
-        1:{"Nombre": "Hotel Boutique ADAZ 4 dias - 3 noches", "Tours": "Tour de Día Completo en Catamarán por el Tayrona", "precio": 755.000},
-        2:{"Nombre": "Titanic Hotel 3 dias - 2 noches", "Tours": "Parque Tayrona - Cabo San Juan", "precio": 395.000},
-        3:{"Nombre": "Hotel Carolina Del Mar 4 dias - 3 noches", "Tour": "Tour Privado Piedras de Donama en Sierra Nevada de Santa Marta", "precio": 999.999}
+        1:{"Nombre": "Hotel Boutique ADAZ 4 dias - 3 noches", "Tours": "Tour de Día Completo en Catamarán por el Tayrona", "precio": 755000},
+        2:{"Nombre": "Titanic Hotel 3 dias - 2 noches", "Tours": "Parque Tayrona - Cabo San Juan", "precio": 395000},
+        3:{"Nombre": "Hotel Carolina Del Mar 4 dias - 3 noches", "Tour": "Tour Privado Piedras de Donama en Sierra Nevada de Santa Marta", "precio": 999999}
     },
     4: {
-        1:{"Nombre": "Hostel Casa de las Palmas 4 dias - 3 noches", "Tours": "Visita Puerto Nariño, Tres Fronteras y la Reserva Marasha ", "precio": 845.000},
-        2:{"Nombre": "Hotel Yurupary 3 dias - 2 noches", "Tours": "Visita la reserva Natural Kindiwaira y la cascada fin del Mundo en Putumayo", "precio": 850.000 }
+        1:{"Nombre": "Hostel Casa de las Palmas 4 dias - 3 noches", "Tours": "Visita Puerto Nariño, Tres Fronteras y la Reserva Marasha ", "precio": 845000},
+        2:{"Nombre": "Hotel Yurupary 3 dias - 2 noches", "Tours": "Visita la reserva Natural Kindiwaira y la cascada fin del Mundo en Putumayo", "precio": 850000 }
     }
 }
 
 import time
 ##Fución para validar que la entrada sea un numero entero
 def validar_entrada(mensaje):
-    entrada = input(mensaje)
+    entrada = input(mensaje).strip()
     if entrada.isdigit():##.isdigit verifica si la cadena es un numero
         return int(entrada)##Retornamos la entrada 
     else:
         print("Ingresa un valor numerico")
-        return validar_entrada(mensaje)##En caso que la entra sea algo diferente a un numero volvemos a llamar la funcion    
+        return validar_entrada(mensaje)##En caso que la entra sea algo diferente a un numero volvemos a llamar la función    
+
+def destinos():
+    print("\033c",end="")
+    print("***Elige un destino***")
+    for clave, valor in vuelos.items():
+        print(f"{clave}. {valor['Ciudad']}.")
+    opcion_destino = validar_entrada("Ingresa una opción: ")
+    return opcion_destino
 ##Creando funciones del menu
 
 def crea_usuario():
@@ -62,15 +70,18 @@ def eliminar_usuario():
     """"Función para eliminar a un usuario usando su Id 
         para buscarlo en el diccionario"""
     print("\033c", end="")##Limpiar consola
-    identificacion = validar_entrada("Ingresa la identificacion del clientes que deseas eliminar: ")
-    ##Validando que la identificacion exista en el diccionario
-    
-    if identificacion in diccionario_usuarios:
-        ##Eliminando al usuario con la funcion pop 
-        diccionario_usuarios.pop(identificacion)
-        print("***El usuario fue eliminado con exito***")
+    ##Validando que el diccionario no es vacio
+    if len(diccionario_usuarios) == 0:
+        print("***No hay usuarios registrados***")
     else:
-        print("***La identificacion no existe***")
+        identificacion = validar_entrada("Ingresa la identificacion del clientes que deseas eliminar: ")
+        ##Validando que la identificacion exista en el diccionario    
+        if identificacion in diccionario_usuarios:
+            ##Eliminando al usuario con la funcion pop 
+            diccionario_usuarios.pop(identificacion)
+            print("***El usuario fue eliminado con exito***")
+        else:
+            print("***La identificacion no existe***")
         
 def mostrar_usuarios():
     """"Función que recorre nuestro diccionario de clientes e imprime en 
@@ -87,31 +98,60 @@ def mostrar_usuarios():
 def crea_paquete_turistico():
     """"comentario sobre la funcion"""
     print("\033c", end="")## Limpiar consola
-
-def busca_paquete_turistico():
-    """comentario sobre la funcion"""
-    print("opcion 3")
+    opcion = destinos()
+    print("Reservar vuelo.")
+    reservar_vuelo(opcion)
+    print("Reservar hotel.")
+    reservar_hotel(opcion)
     
-def reservar_vuelo():
+def reservar_vuelo(clave):
     """comentario sobre la funcion"""
-    print("opcion 4")
-def reservar_hotel():
+    print("\033c", end="")## Limpiar consola
+    if clave in vuelos:
+        destino_elegido = vuelos[clave]["Ciudad"]
+        salidas = vuelos[clave]["salidas"]
+        print(f"El valor de los vuelos para {destino_elegido} es:")
+        for salida, valor in salidas.items():
+            print(f"Salida {salida}: ${valor:,}")
+        opcion_vuelo =input("Ingresa una opción(a,b,c): ").strip().lower()
+        if opcion_vuelo.isdigit() or opcion_vuelo not in {'a','b','c'}:
+            print("***Ingresa una opción valida***")
+            time.sleep(1)
+            print("\033c",end="")
+            reservar_vuelo(clave)                        
+    else:
+        print("***Ingresa una opción valida***")
+        time.sleep(1)
+        return crea_paquete_turistico()
+    
+def reservar_hotel(clave):
     """comentario sobre la funcion"""
-    print("opcion 5")
-
+    print("\033c", end="")## Limpiar consola
+    if clave in hoteles:
+        print("Seleccione una opción de hotel:")
+        for clave_hotel in hoteles[clave]:
+            hotel = hoteles[clave][clave_hotel]
+            print(f"{clave_hotel}. {hotel['Nombre']}")
+        opcion_hotel = validar_entrada("Ingresa una opción: ")
+        if clave in hoteles and opcion_hotel in hoteles[clave]:
+            hotel_elegido = hoteles[clave][opcion_hotel]
+            print(f"Hotel: {hotel_elegido['Nombre']}")
+            print(f"Tours: {hotel_elegido['Tours']}")
+            print(f"Precio: ${hotel_elegido['precio']:,}")
+        else:
+            print("***Ingresa una opción valida***")
+            time.sleep(1)
+            reservar_hotel(clave)  
 def mostrar_compra():
     """comentario sobre la funcion"""
 
 ##Creando diccionario con las diferentes opciones
 menu_opciones = {
-    '1':crea_usuario,
-    '2':eliminar_usuario,
-    '3':mostrar_usuarios,
-    '4':crea_paquete_turistico,
-    '5':busca_paquete_turistico,
-    '6':reservar_vuelo,
-    '7':reservar_hotel,
-    '8':mostrar_compra
+    1:crea_usuario,
+    2:eliminar_usuario,
+    3:mostrar_usuarios,
+    4:crea_paquete_turistico,
+    5:mostrar_compra
 }
 
 ##Creando diccionario vacio para almacenar a los usuarios
@@ -128,18 +168,15 @@ while True:
     print(" 2.Eliminar usuario")
     print(" 3.Mostrar usuarios")
     print(" 4.Crear paquete turistico")
-    print(" 5.Buscar paquetes turisticos")
-    print(" 6.Reservar vuelos")
-    print(" 7.Reservar hotel")
-    print(" 8.Resumen de la compra")
-    print(" 9.Salir")
+    print(" 5.Resumen de la compra")
+    print(" 6.Salir")
     #Solicitando opcion al usuario
-    opcion = input("\n Ingresa una opcion: ")
+    opcion = validar_entrada("\n Ingresa una opcion: ")
     #Invocando Fución escogida por el usuario
     if opcion in menu_opciones:
         menu_opciones[opcion]()
     #Saliendo del menu      
-    elif opcion == '9':
+    elif opcion == 6:
         print("***Gracias por usar nuestro servicio***")
         break
     #Mensaje en caso de que se ingrese un dato equivocado
